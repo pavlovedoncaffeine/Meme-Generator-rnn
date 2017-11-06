bcc17f6784d13e6f84eb76150eab953c841e935c
pavlovedoncaffeine <akash.ravichandran@gmail.com>
2017-11-06 18:00:57 -0500

    Added OCR.py for the first time

--- Desktop/Meme generator rnn/OCR.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image 
-import pytesseract
-import sys
-
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
-print sys.stdout.encoding
-print pytesseract.image_to_string(Image.open('elm.jpg')).encode(errors='replace') 
-##print pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra')
